283deec65716105fb55b4ccd8347d74d4afe00a3
Den <Den@example.com>
2024-05-08 10:35:46 +0300

    remove commented code

--- src/utils/getFileContent.js
+++ src/utils/getFileContent.js
@@ -3,6 +3,5 @@ import fs from "fs/promises";
 export { getFileContent };
 
 async function getFileContent(path) {
-  // const path = new URL("../../config/proxy.json", import.meta.url);
   return await fs.readFile(path, "utf-8");
 }
