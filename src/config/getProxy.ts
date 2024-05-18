6cb8cf4bad30f106ade8a2d9b69ebcef908921a5
Den <Den@example.com>
2024-05-18 12:05:43 +0300

    handle undefined properties in proxy.json

--- src/config/getProxy.ts
+++ src/config/getProxy.ts
@@ -9,5 +9,11 @@ async function getProxy(): Promise<{
 }> {
   const path = new URL("../../config/proxy.json", import.meta.url);
   const fileContent = await fs.readFile(path, "utf-8");
-  return JSON.parse(fileContent);
+  const proxy = JSON.parse(fileContent);
+  if (!proxy.protocol && !proxy.host && !proxy.port) {
+    throw new Error(
+      'proxy config must contain "protocol", "host" and "port" properties',
+    );
+  }
+  return proxy;
 }
